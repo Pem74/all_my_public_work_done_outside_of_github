b1bf8bb1fe6740c5633aabd097d843c2c9031496
Pierre-Emeric Boccard <40424442+Pem74@users.noreply.github.com>
2026-08-27 19:43:20 +0200

    Add files via upload

---
+++ IA/IA04/Projet segmentation/segment_with_canny.py
@@ -0,0 +1,8 @@
+from segmentation import start_segmentation
+
+def segment_with_canny(img):
+    segmented = ...
+    return segmented
+
+if __name__ == '__main__':
+    start_segmentation(segment_with_canny)
