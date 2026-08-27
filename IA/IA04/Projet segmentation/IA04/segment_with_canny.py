699563007afd685682cb687acf1746da78c84c31
Pierre-Emeric Boccard <40424442+Pem74@users.noreply.github.com>
2026-08-27 19:42:08 +0200

    Delete IA/IA04/Projet segmentation/IA04 directory

--- IA/IA04/Projet segmentation/IA04/segment_with_canny.py
+++
@@ -1,8 +0,0 @@
-from segmentation import start_segmentation
-
-def segment_with_canny(img):
-    segmented = ...
-    return segmented
-
-if __name__ == '__main__':
-    start_segmentation(segment_with_canny)
